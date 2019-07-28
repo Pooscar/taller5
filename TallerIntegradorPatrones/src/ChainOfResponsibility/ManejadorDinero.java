1073dbc538fa28201dfbf496124594bf48903258
AllisonB <allisonbarrezueta@gmail.com>
2019-07-27 20:54:22 -0500

    chain package complete, imlemented on main

--- TallerIntegradorPatrones/src/ChainOfResponsibility/ManejadorDinero.java
+++ TallerIntegradorPatrones/src/ChainOfResponsibility/ManejadorDinero.java
@@ -27,23 +27,31 @@ public class ManejadorDinero implements Manejador
         if (monto < 0 || this.cantidad == 0){
             return false;
         }
-        double resto = monto % this.denominacion;
-        if (resto == 0) {
+        double resto = monto / this.denominacion;
+        int valor = (int) resto; // cast double to int
+        if (valor == 0) {
             next.retirar(monto);
         }
-        System.out.println("Se entregan " +resto+" billetes de $"+ this.denominacion);
-        monto -= resto*this.denominacion;
-        this.cantidad -= resto;
+        System.out.println("Se entregan " +valor+" billetes de $"+ this.denominacion);
+        monto -= valor*this.denominacion; // se resta el monto
+        this.cantidad -= valor; // se resta la cantidad de billete
         this.retirar(monto);
+        
         return true;
     }
 
     @Override
     public boolean depositar(int n, double denominacion) {
-        // que la denominacion cumpla tengo que hacer next hasta que encuentre el billete que es
-        //manejadro de dinero usa accout
-        double monto = n*denominacion;
-        System.out.println("Se deposito $" + monto);
-        return true;
+        if (n>0 && denominacion > 0){
+            double monto = n*denominacion;
+            if (this.denominacion == denominacion){
+                System.out.println("Se deposito $" + monto);
+                return true;
+            } else {
+                next.depositar(n, denominacion);
+            }
+            
+        }
+        return false;
     }
 }
--- TallerIntegradorPatrones/src/Patrones/Main.java
+++ TallerIntegradorPatrones/src/Patrones/Main.java
@@ -5,12 +5,31 @@
  */
 package Patrones;
 
+import ChainOfResponsibility.Manejador;
+import ChainOfResponsibility.ManejadorDinero;
+
 public class Main
 {
     public static void main(String[] args)
     {
         // Crear un único cajero Automático de dólares con 100 billetes de 20, 100 de 10, 
         // 10 monedas de 0.50, 10 de 0.25 y 1000 de 0.05
+        ManejadorDinero b20 = null;
+        ManejadorDinero b10 = null;
+        ManejadorDinero m50 = null;
+        ManejadorDinero m25 = null;
+        ManejadorDinero m05 = null;
+        
+        b20.Manejador(100, 20);
+        b10.Manejador(100, 10);
+        m50.Manejador(10, 0.5);
+        m25.Manejador(10, 0.25);
+        m05.Manejador(1000, 0.05);
+        
+        b20.setNext(b10);
+        b10.setNext(m50);
+        m50.setNext(m25);
+        m25.setNext(m05);
 
         // Crear 10 cuentas nuevas en dólares locale.US con un saldo inicial entre 100.00 y 1000.00 USD cada una.
         
